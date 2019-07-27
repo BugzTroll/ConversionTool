d39066ffe95ee564ae13f8d847df7c6b76261412
bugztroll <mariedube2@gmail.com>
2019-07-27 19:39:23 -0400

    adding input and output angle representation

--- javascript/test.js
+++ javascript/test.js
@@ -1,6 +1,7 @@
     let euler = new THREE.Euler(90,0,0);
     let quat = convertEulerToQuaternion(euler);
     let rot = convertEulerToRotationMatrix(euler);
+    let axisAngle = convertEulerToAxisAngle(euler);
 
     function executeTest(){
         console.log("---------------")
@@ -8,6 +9,8 @@
         console.log(convertEulerToQuaternion(euler));
         console.log("euler->rot");
         console.log(convertEulerToRotationMatrix(euler));
+        console.log("euler->axisAngle");
+        console.log(convertEulerToAxisAngle(euler));
     
         console.log("---------------")
         console.log("quat->rot");
@@ -24,6 +27,8 @@
         console.log(convertQuaternionToEuler(quat, "ZYX"));
         console.log("quat->eulerZXY");
         console.log(convertQuaternionToEuler(quat, "ZXY"));
+        console.log("quat->axisAngle");
+        console.log(convertQuaternionToAxisAngle(quat));
     
         console.log("---------------")
         console.log("rot->quat");
@@ -40,6 +45,26 @@
         console.log(convertRotationMatrixToEuler(rot, "ZYX"));
         console.log("rot->eulerZXY");
         console.log(convertRotationMatrixToEuler(rot, "ZXY"));
+        console.log("rot->AxisAngle");
+        console.log(convertRotationMatrixToAxisAngle(rot, "ZXY"));
+
+        console.log("---------------")
+        console.log("AxisAngle->rot");
+        console.log(convertAxisAngleToRotationMatrix(axisAngle));
+        console.log("AxisAngle->quat");
+        console.log(convertAxisAngleToQuaternion(axisAngle));
+        console.log("AxisAngle->eulerXYZ");
+        console.log(convertAxisAngleToEuler(axisAngle, "XYZ"));
+        console.log("AxisAngle->eulerXZY");
+        console.log(convertAxisAngleToEuler(axisAngle, "XZY"));
+        console.log("AxisAngle->eulerYXZ");
+        console.log(convertAxisAngleToEuler(axisAngle, "YXZ"));
+        console.log("AxisAngle->eulerYZX");
+        console.log(convertAxisAngleToEuler(axisAngle, "YZX"));
+        console.log("AxisAngle->eulerZYX");
+        console.log(convertAxisAngleToEuler(axisAngle, "ZYX"));
+        console.log("AxisAngle->eulerZXY");
+        console.log(convertAxisAngleToEuler(axisAngle, "ZXY"));
     }
 
    
